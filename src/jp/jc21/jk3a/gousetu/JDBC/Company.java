ff517e5c486b4ece1ebd1e88d9eef59042b7b616
毅 吉澤 <t.yoshizawa@jc-21.jp>
2019-12-11 15:55:49 +0900

    Companyクラスの修正

--- src/jp/jc21/jk3a/gousetu/JDBC/Company.java
+++ src/jp/jc21/jk3a/gousetu/JDBC/Company.java
@@ -1,9 +1,13 @@
 package jp.jc21.jk3a.gousetu.JDBC;
 
-import jp.jc21.jk3a.gousetu.gousetuDbInterface;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import jp.jc21.jk3a.gousetu.gousetuDbInterface;
 
 public class Company implements gousetuDbInterface {
 
